c32e4a2f1eb00edb38f4f8d408b27b137eef56da
iqDF <daniel.thekurniadi@gmail.com>
2019-01-06 13:35:12 +0800

    add unit tests and implement new-user test

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewUserTests(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_startAppLists(self):
+        # Edith has heard about a cool new online to-do app. She goes         
+        # to check out its homepage         
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists 
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finishing unit test...')
+
+        # She is invited to enter to-do items
+
+
+if __name__ == "__main__":
+    unittest.main(warnings='ignore')
