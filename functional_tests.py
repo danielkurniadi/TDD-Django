deeab282763542017b2fa36485b581e151bd7d05
iqDF <daniel.thekurniadi@gmail.com>
2019-01-07 13:41:45 +0800

    Sasha now can add first item and see it in table of todo lists

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewUserTests(unittest.TestCase):
@@ -9,17 +11,46 @@ class NewUserTests(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_startAppLists(self):
-        # Edith has heard about a cool new online to-do app. She goes         
+    def test_todoFeatures(self):
+        # Sasha has heard about a cool new online to-do app. She goes         
         # to check out its homepage         
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists 
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finishing unit test...')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter to-do items
+        inputBox = self.browser.find_element_by_id('newitem-id')
+        self.assertEqual(
+            inputBox.get_attribute('placeholder'),
+            'Enter To-Do item'
+        )
 
+        # She typed: "Buy peacock-leathered jacket"
+        inputBox.send_keys("Buy peacock-leathered jacket")
+        inputBox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # When she hits enter, the To-Do lists has her newly added items
+        # Now the page lists has: "1. Buy peacock-leathered jacket"
+        table = self.browser.find_element_by_id('todo-table-id')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn("1. Buy peacock-leathered jacket", [row.text for row in rows])
+        
+        # She then add another items, she wanted to show off...
+        inputBox.send_keys("Show off the jacket to friends")
+        inputBox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # Now page lists has "2. Show off jacket to friends"
+        table = self. browser.find_element_by_id('todo-table-id')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn("2. Show off the jacket to friends", [row.text for row in rows])
+
+        # Sasha is satisfied 
+        self.fail('Finishing unit test...')
 
 if __name__ == "__main__":
     unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,24 @@
 from django.test import TestCase
 from django.urls import resolve 
+from django.http import HttpRequest
 from lists.views import home_page
+from django.template.loader import render_to_string
+
+
 
 # Create your tests here.
 class HomePageTest(TestCase):
 
     def test_resolveRootUrl(self):
+        #resolve root URL as home page
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_homePageContent(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+    
+    def test_POSTRequest(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
